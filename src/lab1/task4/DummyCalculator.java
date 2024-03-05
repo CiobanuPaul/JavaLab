39780d67a3c3d653e45238930470b93bcf54b64c
CiobanuPaul <ciobanuioanpaul43@gmail.com>
2024-03-05 16:42:05 +0200

    Task 4 finished

--- src/lab1/task4/DummyCalculator.java
+++ src/lab1/task4/DummyCalculator.java
@@ -1,8 +1,14 @@
 package lab1.task4;
 
+import java.util.Objects;
+
 public class DummyCalculator {
 
-    public String detectType(String input) {
+
+    private static int strength(String type){
+        return Objects.equals(type, "float") ? 2 : 1;
+    }
+    private static String detectType(String input) {
         try {
             int intValue = Integer.parseInt(input);
             return "int";
@@ -19,11 +25,57 @@ public class DummyCalculator {
             }
         }
     }
-    public void calculate(String[] args){
-        var type = detectType(args[0]);
-        switch (type){
+    public static void calculate(String[] args) {
+        var type1 = detectType(args[0]);
+        var type2 = detectType(args[2]);
+        String type;
+        if (strength(type1) > strength(type2))
+            type = type1;
+        else
+            type = type2;
+
+        switch (type) {
             case "int":
-                if(args[1] == "+")
+                switch(args[1]){
+                    case "+":
+                        System.out.println(Integer.parseInt(args[0]) + Integer.parseInt(args[2]));
+                        break;
+                    case "-":
+                        System.out.println(Integer.parseInt(args[0]) - Integer.parseInt(args[2]));
+                        break;
+                    case "*":
+                        System.out.println(Integer.parseInt(args[0]) * Integer.parseInt(args[2]));
+                        break;
+                    case "/":
+                        System.out.println(Integer.parseInt(args[0]) / Integer.parseInt(args[2]));
+                        break;
+                }
+                break;
+            case "float":
+                switch(args[1]){
+                    case "+":
+                        System.out.println(Float.parseFloat(args[0]) + Float.parseFloat(args[2]));
+                        break;
+                    case "-":
+                        System.out.println(Float.parseFloat(args[0]) - Float.parseFloat(args[2]));
+                        break;
+                    case "*":
+                        System.out.println(Float.parseFloat(args[0]) * Float.parseFloat(args[2]));
+                        break;
+                    case "/":
+                        System.out.println(Float.parseFloat(args[0]) / Float.parseFloat(args[2]));
+                        break;
+                }
+                break;
+            case "boolean":
+                switch(args[1]){
+                    case "&":
+                        System.out.println(Boolean.parseBoolean(args[0]) & Boolean.parseBoolean(args[2]));
+                        break;
+                    case "|":
+                        System.out.println(Boolean.parseBoolean(args[0]) | Boolean.parseBoolean(args[2]));
+                        break;
+                }
         }
     }
 }
